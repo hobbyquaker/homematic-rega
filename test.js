e9f9a49ef2057a4a4fa4afc76e2fd099a38925f0
hobbyquaker <bastiraff@googlemail.com>
2018-05-05 21:57:49 +0200

    test getRooms

--- test.js
+++ test.js
@@ -1,16 +1,20 @@
 const Rega = require('./index.js');
-const rega = new Rega({host: '172.16.23.175'});
-
+const rega = new Rega({host: '172.16.23.175', disableTranslation: false});
 
 rega.exec('string x = "Hello";\nWriteLine(x # " World!");', (err, output, objects) => {
     if (err) {
         throw err;
     }
-    console.log('Output:', output);
-    console.log('Objects:', objects);
-});
+    console.log('Script Output:', output);
+    console.log('Script Objects:', objects);
 
+    rega.getVariables((err, res) => {
+        console.log('getVariables', err, res);
 
-rega.getVariables((err, res) => {
-    console.log(err, res);
+        rega.getRooms((err, res) => {
+            console.log('getRooms', err, res);
+        });
+
+    });
+
 });

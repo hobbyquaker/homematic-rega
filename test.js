b85433c6e1b024d880f3666baeb1963f3083fe05
hobbyquaker <bastiraff@googlemail.com>
2019-04-13 16:42:56 +0200

    create channels and values file

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const Rega = require('./index.js');
-const rega = new Rega({host: 'homematic-raspi', disableTranslation: false});
+const rega = new Rega({host: 'homematic-ccu3', disableTranslation: false});
 
 
 /*
@@ -35,8 +35,21 @@ rega.exec('string x = "Hello";\nWriteLine(x # " World!");', (err, output, object
     });
 
 });
+
+
+
 */
 
-rega.getVariables((err, res) => {
-    console.log('getVariables', err, res);
+rega.getChannels((err, res) => {
+    console.log(err);
+    require('fs').writeFileSync('channels.json', JSON.stringify(res, null, '  '));
+
+    rega.getValues((err, res) => {
+        console.log(err);
+        require('fs').writeFileSync('values.json', JSON.stringify(res, null, '  '));
+    });
 });
+
+
+
+
